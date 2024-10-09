d03a4bda651b43f2aa9adbbe676b9e4feaf65e23
Ibra-Hud <ibrahimhudson61@gmail.com>
2024-10-09 15:54:59 -0400

    generic update looks

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Testing JavaScript in HTML
-console.log('Hello World');
-let name = 'Ibrahim'
+console.log("Hello World");
+let name = "Ibrahim";
 
 let check = (num1, num2) => {
-    if (num1 === num2){
-        console.log("Correct")
-    }
-}
+  if (num1 === num2) {
+    console.log("Correct");
+  }
+};
 
-check(1, "1")
+check(1, "1");
